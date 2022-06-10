d69316a7488d89523c0b2a791de46b6e9b9bf3c8
Julie Van Audenhaege <rvan-aud@fu-r9-p5.s19.be>
2022-06-10 17:36:10 +0200

    first push

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RootModule } from './modules/root.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RootModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
