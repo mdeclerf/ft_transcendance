b1f2d7d76dd196c4d8de076d9966b05093ca7015
Julie Van Audenhaege <rvan-aud@fu-r9-p5.s19.be>
2022-06-28 17:38:35 +0200

    chat ok

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RootModule } from './modules/root.module';
-
-@Module({
-  imports: [RootModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
