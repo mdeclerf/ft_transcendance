128ce97c0161bf8b9d89c7b23243788899c1c50a
viforget <victor.forget99@gmail.com>
2022-07-14 14:41:35 +0200

    Add chat user table in the database + services functions

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
 import { FriendlistModule } from './friendlist/friendlist.module';
+import { ChatUserModule } from './chat_user/chat_user.module';
 
 @Module({
-	imports: [ChatModule, FriendlistModule],
+	imports: [ChatModule, FriendlistModule, ChatUserModule],
 })
 export class ApiModule {}
