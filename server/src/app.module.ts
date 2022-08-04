922f9dd016d9a83eb84c0b28b4249cfb8d6f1f37
Julie Van Audenhaege <rvan-aud@fu-r5-p5.s19.be>
2022-08-04 17:58:03 +0200

    fixed indentation but back is still not working

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -25,7 +25,7 @@ import { ChatGateway } from './chat/chat.gateway'
       synchronize: true,
     }),
     UserModule,
-	OrmModule
+	  OrmModule
   ],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
