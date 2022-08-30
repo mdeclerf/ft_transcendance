98496a183a9ee1b1d32d6267759f68994197a007
Julie Van Audenhaege <rvan-aud@fu-r9-p8.s19.be>
2022-08-30 14:39:25 +0200

    changed mute to muted in back

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -95,7 +95,8 @@ export class ChatGateway
 	async messageSend(client: Socket, message: CreateChatDto) {
 		const currentRoom = await this.chatService.getRoomByName(message.room.name);
 		const status = await this.chatService.getChatUserStatus(message.user, currentRoom);
-		if (status !== "mute")
+		console.log(status);
+		if (status !== "muted")
 		{
 			this.chatService.createMessage(message);
 			const { room } = message;
